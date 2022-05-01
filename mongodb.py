6f4afd89aa88b643eab785b23192696c101a9913
Matthew Aylward <matthewtyler.aylward@uzh.ch>
2022-05-01 20:29:37 +0200

    🥸: Updated to hide the secrets

--- mongodb.py
+++ mongodb.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 
 client = MongoClient(
-    "mongodb+srv://codingkitten:pgbZrSeXYgtrz7II@cluster0.8dgma.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    "mongodb+srv://codingkitten:<password>@cluster0.8dgma.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     server_api=ServerApi("1"),
 )
 
