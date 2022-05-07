1e8b8fb7e820b0e0d4b47989cb4a72987cf81bed
diegobugmann <dibugm@DESKTOP-UQ0V5QQ.localdomain>
2022-05-07 17:41:12 +0200

    improved testing

--- app/tests/test_parser.py
+++ app/tests/test_parser.py
@@ -1,10 +1,17 @@
+from typing import List
+
 import libcst as cst
 from app.services.parser import Parser
 
 
-def test_pass_module(parser: Parser):
+def test_parse_module(parser: Parser):
 
     user_input = "if 1 == 1: pass"
-    module = parser.parse_module(user_input)
 
+    module = cst.parse_module(user_input)
+    # the parse_module function from libcst returns Module
     assert isinstance(module, cst.Module)
+
+    module = parser.parse_module(user_input)
+    # the parse_module function from our parser returns a list of JSON objects
+    assert isinstance(module, List)
