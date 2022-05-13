985f99e947450983ab030faa3e1fd68c684c7344
diegobugmann <diegoricardo.bugmann@uzh.ch>
2022-05-14 00:45:50 +0200

    fixed JSON tests

--- app/tests/test_nodetojson.py
+++ app/tests/test_nodetojson.py
@@ -71,20 +71,14 @@ def test_assign(parser: Parser):
     command1_parsed = parser.parse_module(byte_command_1)
     command2_parsed = parser.parse_module(byte_command_2)
 
-    assert command1_parsed[0]["type"] == "Assign"
-    assert command1_parsed[0]["left"] == "a"
-    assert command1_parsed[0]["right"] == "3"
+    assert command1_parsed[0]["type"] == "Line"
     assert command1_parsed[0]["command"] == "a = 3"
     assert len(command1_parsed) == 1
 
-    assert command2_parsed[0]["type"] == "Assign"
-    assert command2_parsed[0]["left"] == "a"
-    assert command2_parsed[0]["right"] == "4"
+    assert command2_parsed[0]["type"] == "Line"
     assert command2_parsed[0]["command"] == "a = 4"
 
-    assert command2_parsed[1]["type"] == "Assign"
-    assert command2_parsed[1]["left"] == "b"
-    assert command2_parsed[1]["right"] == "c"
+    assert command2_parsed[1]["type"] == "Line"
     assert command2_parsed[1]["command"] == "b = c"
     assert len(command2_parsed) == 2
 
@@ -100,15 +94,11 @@ def test_aug_assign(parser: Parser):
     command1_parsed = parser.parse_module(byte_command_1)
     command2_parsed = parser.parse_module(byte_command_2)
 
-    assert command1_parsed[0]["type"] == "AddAssign"
-    assert command1_parsed[0]["left"] == "a"
-    assert command1_parsed[0]["right"] == "7"
+    assert command1_parsed[0]["type"] == "Line"
     assert command1_parsed[0]["command"] == "a += 7"
     assert len(command1_parsed) == 1
 
-    assert command2_parsed[0]["type"] == "MultiplyAssign"
-    assert command2_parsed[0]["left"] == "b"
-    assert command2_parsed[0]["right"] == "2"
+    assert command2_parsed[0]["type"] == "Line"
     assert command2_parsed[0]["command"] == "b *= 2"
     assert len(command2_parsed) == 1
 
@@ -124,14 +114,10 @@ def test_assign_binary(parser: Parser):
     command1_parsed = parser.parse_module(byte_command_1)
     command2_parsed = parser.parse_module(byte_command_2)
 
-    assert command1_parsed[0]["type"] == "Assign"
-    assert command1_parsed[0]["left"] == "a"
-    assert command1_parsed[0]["right"] == "b + 1"
+    assert command1_parsed[0]["type"] == "Line"
     assert command1_parsed[0]["command"] == "a = b + 1"
 
-    assert command2_parsed[0]["type"] == "ModuloAssign"
-    assert command2_parsed[0]["left"] == "c"
-    assert command2_parsed[0]["right"] == "t - 4"
+    assert command2_parsed[0]["type"] == "Line"
     assert command2_parsed[0]["command"] == "c %= t - 4"
 
 
@@ -149,21 +135,15 @@ def test_comparison(parser: Parser):
     command2_parsed = parser.parse_module(byte_command_2)
     command3_parsed = parser.parse_module(byte_command_3)
 
-    assert command1_parsed[0]["type"] == "Comparison.Equal"
-    assert command1_parsed[0]["left"] == "a"
-    assert command1_parsed[0]["right"] == "3"
+    assert command1_parsed[0]["type"] == "Line"
     assert command1_parsed[0]["command"] == "a == 3"
     assert len(command1_parsed) == 1
 
-    assert command2_parsed[0]["type"] == "Comparison.LessThan"
-    assert command2_parsed[0]["left"] == "b"
-    assert command2_parsed[0]["right"] == "5"
+    assert command2_parsed[0]["type"] == "Line"
     assert command2_parsed[0]["command"] == "b < 5"
     assert len(command2_parsed) == 1
 
-    assert command3_parsed[0]["type"] == "Comparison.GreaterThanEqual"
-    assert command3_parsed[0]["left"] == "c"
-    assert command3_parsed[0]["right"] == "f"
+    assert command3_parsed[0]["type"] == "Line"
     assert command3_parsed[0]["command"] == "c >= f"
     assert len(command3_parsed) == 1
 
@@ -179,15 +159,11 @@ def test_boolean_operation(parser: Parser):
     command1_parsed = parser.parse_module(byte_command_1)
     command2_parsed = parser.parse_module(byte_command_2)
 
-    assert command1_parsed[0]["type"] == "And"
-    assert command1_parsed[0]["left"] == "x"
-    assert command1_parsed[0]["right"] == "y"
+    assert command1_parsed[0]["type"] == "Line"
     assert command1_parsed[0]["command"] == "x and y"
     assert len(command1_parsed) == 1
 
-    assert command2_parsed[0]["type"] == "Or"
-    assert command2_parsed[0]["left"] == "a"
-    assert command2_parsed[0]["right"] == "b"
+    assert command2_parsed[0]["type"] == "Line"
     assert command2_parsed[0]["command"] == "a or b"
     assert len(command2_parsed) == 1
 
@@ -209,27 +185,19 @@ def test_binary_operation(parser: Parser):
     command3_parsed = parser.parse_module(byte_command_3)
     command4_parsed = parser.parse_module(byte_command_4)
 
-    assert command1_parsed[0]["type"] == "Add"
-    assert command1_parsed[0]["left"] == "1"
-    assert command1_parsed[0]["right"] == "2"
+    assert command1_parsed[0]["type"] == "Line"
     assert command1_parsed[0]["command"] == "1 + 2"
     assert len(command1_parsed) == 1
 
-    assert command2_parsed[0]["type"] == "Multiply"
-    assert command2_parsed[0]["left"] == "4"
-    assert command2_parsed[0]["right"] == "b"
+    assert command2_parsed[0]["type"] == "Line"
     assert command2_parsed[0]["command"] == "4 * b"
     assert len(command2_parsed) == 1
 
-    assert command3_parsed[0]["type"] == "Divide"
-    assert command3_parsed[0]["left"] == "c"
-    assert command3_parsed[0]["right"] == "d"
+    assert command3_parsed[0]["type"] == "Line"
     assert command3_parsed[0]["command"] == "c / d"
     assert len(command3_parsed) == 1
 
-    assert command4_parsed[0]["type"] == "Power"
-    assert command4_parsed[0]["left"] == "a"
-    assert command4_parsed[0]["right"] == "2"
+    assert command4_parsed[0]["type"] == "Line"
     assert command4_parsed[0]["command"] == "a ** 2"
     assert len(command4_parsed) == 1
 
