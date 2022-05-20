909c000affd4f80b8e2acac442152139d98a32df
diegobugmann <diegoricardo.bugmann@uzh.ch>
2022-05-20 21:58:37 +0200

    updated all comments for PUT (almost ready code)

--- app/api/api_v1/command.py
+++ app/api/api_v1/command.py
@@ -4,18 +4,15 @@ from typing import List
 
 # CRUD operations for the Command model
 from app import crud
-
 # Models
 from app.models.command import Command, CommandCreate, UserInput
 from app.models.edge import Edge
 from app.models.node import Node
 from app.models.variable import Variable
-
 # Services
 from app.services.edge_creator import EdgeCreator
 from app.services.executor import Executor
 from app.services.jupyter_executor import ExecutorJuypter
-
 # Fastapi Dependencies
 from app.services.parser import Parser
 from aredis_om.model import NotFoundError
@@ -84,8 +81,7 @@ async def save_command(
         )
 
     # create and save nodes
-    # TODO
-
+    # TODO ensure nodes have an ID
     db_nodes = await crud.node.create_bulk(
         nodes=nodes, parent_pk=None, command_pk=db_command.pk
     )
@@ -128,34 +124,59 @@ async def get_command(pk: str, request: Request, response: Response):
         raise HTTPException(status_code=404, detail="Command not found")
 
 
+# TODO make it so that token, nodeID and commandwrapperID are sent from frontend
 # 1. User changes a node and sends a request including NodeID, respective CommandWrapperID and new command (for the node)
 @router.put("/{pk}", response_model=Command)
 async def put_command(pk: str, command: Command, response: Response) -> Command:
 
-    # 2. Fetch ALL session wrappers
-    # session_wrappers = ... some db call ...
 
-    # 3. Fetch the modified CommandWrapper given the CommandWrapper ID
-    # modified_wrapper = LOOK FOR CORRECT ONE IN "session_wrappers"
+    # 2. Fetch ALL session wrappers ✅
+    # session_wrappers = await crud.command.read_all_by_token(userinput.token)
 
-    # 4. Create a list of successors and predecessor (also needed later on)
-    # successors = session_wrappers AFTER THE MODIFIED ONE
-    # predecessors = session_wrappers BEFORE THE MODIFIED ONE
 
-    # 5. Call a function (Grigor’s work) to create a new TEMPORARY JSON-tree (for the “nodes” of the commandwrapper) with the updates made
-    # current_nodes = modified_wrapper GET "nodes" JSON
-    # new_nodes = CALL FUNTION
+    # 3. Fetch the modified CommandWrapper given the CommandWrapper ID ✅
+    # for i in range(len(session_wrappers)):
+    #     if wrapper.pk == userinput.wrapperid:
+    #         edit_index = i
+    #         modified_wrapper = session_wrappers[i]
 
-    # 6. Create the updated command property of the CommandWrapper by calling a function that creates the code from the temporary JSON tree (Grigor’s work)
-    # new_commandwrapper_command = create_python_code(new_nodes) -> CALL FUNTION
 
-    # 7. Recreate edges (again, just temporarily save them)
-    # new_edges = edge_creator.create_edges(new_nodes)
+    # 4. Create a list of successors and predecessor (also needed later on) ✅
+    # predecessors = session_wrappers[0:edit_index]
+    # successors = session_wrappers[edit_index+1:]
 
-    # 8. Fetch the state of the variables from the PREVIOUS commandwrapper (the one before the modified one)
-    # latest_variables = predecessors[-1].variables
 
-    # 9. Call the exec function for the temporary command of the CommandWrapper with the previously fetched variables as input and temporarily save the variables.
+    # 5. Create a updated json tree ✅
+    # current_nodes = modified_wrapper.nodes
+    # nodeEditor = NodeEditor(current_nodes)
+    # nodeEditor.edit_node(
+    #     node_id=userinput.nodeid,
+    #     new_command=userinput.newcommand
+    # )
+    # new_nodes = nodeEditor.json
+
+
+    # 6. Create the updated command property of the CommandWrapper ✅
+    # jsonToSourceCodeConverter = JSONToSourceCodeConverter(new_nodes)
+    # new_commandwrapper_command = jsonToSourceCodeConverter.generate_source_code()
+
+
+    # 7. Recreate edges (again, just temporarily save them) ✅
+    # edgeCreator = EdgeCreator(json_data=new_nodes)
+    # edgeCreator.create_edges()
+    # new_edges = edgeCreator.edges
+
+
+    # 8. Fetch the state of the variables from the previous commandwrapper (if there is one) ✅
+    # previous_wrapper = predecessors[-1] if predecessors else None
+    # latest_variables = {}
+    # if previous_wrapper:
+    #     for x in previous_wrapper.variables:
+    #         value = int(x.value) if x.value.isnumeric() else x.value
+    #         latest_variables[x.var_name] = value
+
+
+    # 9. Call the exec function for the temporary (new) command of the CommandWrapper ✅
     # try:
     #     new_variables = executor.exec_module_from_history(
     #         new_commandwrapper_command,
@@ -164,7 +185,8 @@ async def put_command(pk: str, command: Command, response: Response) -> Command:
     # except Exception as e:
     #     return {"error": str(e)}
 
-    # 10. Now for all successor CommandWrappers, execute again
+
+    # 10. Now for all successor CommandWrappers, execute again and store the vars in a list ✅
     # temp_vars = [] # create a list to store the variables for each successor wrapper temporarily
     # current_vars = new_variables # start by passing the just fetched vars from the modified wrapper
     # try:
@@ -178,28 +200,32 @@ async def put_command(pk: str, command: Command, response: Response) -> Command:
     # except Exception as e:
     #     return {"error": str(e)}
 
-    # 11. Now combine ALL commands (including the updated one) and execute them in a Jupyter notebook to receive the outputs
+
+    # 11. Now combine ALL commands (including the updated one) and execute them in a Jupyter notebook to receive the outputs ✅
     # predecessor_commands = [wrapper.command for wrapper in predecessors]
     # successor_commands = [wrapper.command for wrapper in successors]
     # all_wrapper_commands = predecessor_commands + new_commandwrapper_command + successor_commands
-
     # try:
     #     all_outputs = run_notebook_given_history(all_wrapper_commands)
     # except Exception as e:
     #     return {"error": str(e)}
-    # relevant_outputs = all_outputs[-(1+len(successors)):] # fetch all outputs for the modified_wrapper and all successors
+    # relevant_outputs = all_outputs[-(1+len(successors)):] # fetch the outputs for the modified_wrapper and all successors
+
 
     # 12. NO ERROR WAS THROWN SO FAR -> Create new CommandWrappers and delete the old ones
     # Start with the initially modified one:
-    # CREATE "modified_wrapper" WITH "new_nodes", "new_commandwrapper_command", "new_edges", "new_variables", "relevant_outputs[0]"
-    # REPLACE "modified_wrapper" with the newly created wrapper
+    # TODO CREATE wrapper WITH "new_nodes", "new_commandwrapper_command", "new_edges", "new_variables", "relevant_outputs[0]" (CREATED_AT, PK and TOKEN NEED TO STAY THE SAME)
+    # TODO REPLACE "modified_wrapper" with the newly created wrapper
+
 
     # and then all the successors (CREATE NEW ONES, REPLACING THE VARIABLES AND OUTPUTS, REST SHOULD STAY THE SAME)
     # for i in range(len(successors)):
-    #     CREATE NEW WRAPPER with "temp_vars[i]" for variables, "relevant_outputs[i+1]" for outputs and all other parameters from successors[i]
-    #     REPLACE successors[i] with the newly created wrapper
+    #     TODO CREATE NEW WRAPPER with "temp_vars[i]" for variables, "relevant_outputs[i+1]" for outputs and ALL OTHER PARAMETERS FROM successors[i] (NEED TO STAY THE SAME)
+    #     TODO REPLACE successors[i] with the newly created wrapper
+
 
     # 13. SAVE ALL THE NEWLY CREATED WRAPPERS AND DELETE THE OLD ONES
+
 
     # 14. RETURN THE HISTORY OF ALL SESSION WRAPPERS
 
