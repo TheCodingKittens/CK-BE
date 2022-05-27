80900ae1e610b36e4116f90f5d27915c41b6f416
diegobugmann <diegoricardo.bugmann@uzh.ch>
2022-05-27 20:59:41 +0200

    added command_controller logic

--- app/controllers/command_controller.py
+++ app/controllers/command_controller.py
@@ -7,7 +7,13 @@ from typing import List
 
 from app import crud
 from app.models.base64 import Base64Type
-from app.models.command import Command, CommandCreate, UserInput, UserInputDelete
+from app.models.command import (
+    Command,
+    CommandCreate,
+    UserInput,
+    UserInputDelete,
+    UserInputSwap,
+)
 from app.models.edge import Edge
 from app.models.node import Node, NodeRead
 from app.models.variable import Variable
@@ -231,6 +237,61 @@ class CommandController:
                 break
 
         session_commands.pop(delete_index)
+
+        # 3. Execute them in a Jupyter notebook to receive the outputs (why here? only run once!) ✅
+        all_wrapper_commands = [wrapper.command for wrapper in session_commands]
+        all_outputs = jupyter_executor.run_notebook_given_history(all_wrapper_commands)
+
+        # 4. Run & Save them to the database with a new token
+        temp_token = str(uuid.uuid4())
+
+        for i, command in enumerate(session_commands):
+            await self.save(
+                user_input=UserInput(token=temp_token, command=command.command),
+                parser=parser,
+                executor=executor,
+                jupyter_executor=jupyter_executor,
+                variable_transformer=variable_transformer,
+                output=all_outputs[i],
+            )
+
+        # 5. No error -> Delete the existing command_wrappers
+        await crud.command.delete_all_by_token(user_input.token)
+
+        # 6. Update the temp token to the original token
+        await crud.command.update_tokens(
+            temp_token=temp_token, existing_token=user_input.token
+        )
+
+        # 7. Return the updated session history
+        return await crud.command.read_all_by_token(token=user_input.token)
+
+    # ------------------------ SWAP ------------------------
+    async def swap(
+        self,
+        user_input: UserInputSwap,
+        parser: Parser,
+        executor: Executor,
+        jupyter_executor: ExecutorJuypter,
+        variable_transformer: VariableTransformer,
+    ) -> List[Command]:
+
+        # 1. Fetch ALL session wrappers ✅
+        session_commands = await crud.command.read_all_by_token(user_input.token)
+
+        # 2. Swap the two wrappers given their ID's ✅
+        wrapper_1_index = 0
+        wrapper_2_index = 0
+        for i, command in enumerate(session_commands):
+            if command.pk == user_input.wrapper_id_1:
+                wrapper_1_index = i
+            if command.pk == user_input.wrapper_id_2:
+                wrapper_2_index = i
+
+        session_commands[wrapper_1_index], session_commands[wrapper_2_index] = (
+            session_commands[wrapper_2_index],
+            session_commands[wrapper_1_index],
+        )
 
         # 3. Execute them in a Jupyter notebook to receive the outputs (why here? only run once!) ✅
         all_wrapper_commands = [wrapper.command for wrapper in session_commands]
