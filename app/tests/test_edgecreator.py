f938daa874e0bef9bc984031c71c1fe102c39b93
diegobugmann <diegoricardo.bugmann@uzh.ch>
2022-05-29 21:56:26 +0200

    added edge_creator tests

--- app/tests/test_edgecreator.py
+++ app/tests/test_edgecreator.py
@@ -11,155 +11,84 @@ def create_bytecode(command: str) -> Base64Type:
 
 def test_edge_creation(parser: Parser):
     source_code = """
-a = 2
-b = 3
-
-if a > 2:
-    if a > 4:
-        b = 5
-    a = 4
-
-if b > 20:
-    while a > 10:
-        a = 1
-    c = 19
-
-c = 1
-
-if a > b:
-    a = 100
-    for i in range(10):
-        print(i)
-
-isValid = False
-    """
+numbers = [1, 2, 7, 8, 12, 4, 5, 9]
+while numbers:
+    number = numbers.pop()
+    print(number)
+for i in range(20):
+    if i == 10:
+        print('yay!')
+    else:
+        print('ney!')
+"""
 
     byte_code = create_bytecode(source_code)
     json_objects = parser.parse_module(byte_code)
 
     edgeCreator = EdgeCreator(json_data=json_objects)
     edgeCreator.create_edges()
-    edgeCreator.create_readable_edges(show_ids=False)
 
     correct_edges = [
-        {"from": "a = 2", "to": "b = 3"},
-        {"from": "b = 3", "to": "if a > 2:"},
-        {"from": "if a > 2:", "to": "If.body"},
-        {"from": "if a > 2:", "to": "if b > 20:"},
-        {"from": "If.body", "to": "if b > 20:"},
-        {"from": "if a > 4:", "to": "If.body", "parent": "If.body"},
-        {"from": "if a > 4:", "to": "a = 4", "parent": "If.body"},
-        {"from": "If.body", "to": "a = 4", "parent": "If.body"},
-        {"from": "b = 5", "to": None},
-        {"from": "a = 4", "to": None},
-        {"from": "if b > 20:", "to": "If.body"},
-        {"from": "if b > 20:", "to": "c = 1"},
-        {"from": "If.body", "to": "c = 1"},
-        {"from": "while a > 10:", "to": "While.body", "parent": "If.body"},
-        {"from": "while a > 10:", "to": "c = 19", "parent": "If.body"},
-        {"from": "While.body", "to": "while a > 10:", "parent": "If.body"},
-        {"from": "a = 1", "to": None},
-        {"from": "c = 19", "to": None},
-        {"from": "c = 1", "to": "if a > b:"},
-        {"from": "if a > b:", "to": "If.body"},
-        {"from": "if a > b:", "to": "isValid = False"},
-        {"from": "If.body", "to": "isValid = False"},
-        {"from": "a = 100", "to": "for i in range(10):", "parent": "If.body"},
-        {"from": "for i in range(10):", "to": "For.body", "parent": "If.body"},
-        {"from": "For.body", "to": "for i in range(10):", "parent": "If.body"},
-        {"from": "print(i)", "to": None},
-        {"from": "isValid = False", "to": None},
+        {
+            "from": "numbers = [1, 2, 7, 8, 12, 4, 5, 9]",
+            "to": "while numbers:",
+            "parent": None,
+            "executed": True,
+        },
+        {
+            "from": "while numbers:",
+            "to": "While.body",
+            "parent": None,
+            "executed": True,
+        },
+        {
+            "from": "while numbers:",
+            "to": "for i in range(20):",
+            "parent": None,
+            "executed": True,
+        },
+        {
+            "from": "While.body",
+            "to": "while numbers:",
+            "parent": None,
+            "executed": True,
+        },
+        {
+            "from": "number = numbers.pop()",
+            "to": "print(number)",
+            "parent": "While.body",
+            "executed": True,
+        },
+        {
+            "from": "for i in range(20):",
+            "to": "For.body",
+            "parent": None,
+            "executed": True,
+        },
+        {
+            "from": "For.body",
+            "to": "for i in range(20):",
+            "parent": None,
+            "executed": True,
+        },
+        {
+            "from": "if i == 10:",
+            "to": "If.body",
+            "parent": "For.body",
+            "executed": True,
+        },
+        {"from": "if i == 10:", "to": "else:", "parent": "For.body", "executed": False},
     ]
-
-    assert correct_edges == edgeCreator.readable_edges
-
-
-def test_parent_assignment(parser: Parser):
-    source_code = """
-if a > b:
-    if c > d:
-        for i in range(10):
-            a = 2
-            b = 3
-            c = 4
-    """
-
-    byte_code = create_bytecode(source_code)
-    json_objects = parser.parse_module(byte_code)
-
-    edgeCreator = EdgeCreator(json_data=json_objects)
-    edgeCreator.create_edges()
-    edgeCreator.create_readable_edges(show_ids=False)
-
-    parents = []
 
     for edge in edgeCreator.readable_edges:
-        if "parent" in edge:
-            parents.append(edge)
+        if not edge["parent"]:
+            edge["parent"] = None
 
-    correct_parents = [
-        {"from": "if c > d:", "to": "If.body", "parent": "If.body"},
-        {"from": "for i in range(10):", "to": "For.body", "parent": "If.body"},
-        {"from": "For.body", "to": "for i in range(10):", "parent": "If.body"},
-        {"from": "a = 2", "to": "b = 3", "parent": "For.body"},
-        {"from": "b = 3", "to": "c = 4", "parent": "For.body"},
-    ]
+        if edge["executed"] == "true":
+            edge["executed"] = True
+        else:
+            edge["executed"] = False
 
-    assert correct_parents == parents
-
-
-def test_executed_edges(parser: Parser):
-    source_code = """
-a = 4
-b = 2
-if a > b:
-    print('a is larger than b!')
-else:
-    print('a is smaller than :(')
-finished = True
-    """
-
-    byte_code = create_bytecode(source_code)
-    json_objects = parser.parse_module(byte_code)
-
-    edgeCreator = EdgeCreator(json_data=json_objects)
-    edgeCreator.create_edges()
-    edgeCreator.create_readable_edges(show_ids=False)
-
-    correct_edges = [
-        {"from": "a = 4", "to": "b = 2", "executed": True},
-        {"from": "b = 2", "to": "if a > b:", "executed": True},
-        {"from": "if a > b:", "to": "If.body", "executed": True},
-        {"from": "if a > b:", "to": "else:", "executed": False},
-        {"from": "If.body", "to": "finished = True", "executed": False},
-        {"from": "else:", "to": "finished = True", "executed": False},
-    ]
-
-    assert correct_edges == edgeCreator.readable_edges
-
-
-def test_executed_edges(parser: Parser):
-    source_code = """
-a = 4
-b = 2
-if a > b:
-    print('a is larger than b!')
-    """
-
-    byte_code = create_bytecode(source_code)
-    json_objects = parser.parse_module(byte_code)
-
-    edgeCreator = EdgeCreator(json_data=json_objects)
-    edgeCreator.create_edges()
-    edgeCreator.create_readable_edges(show_ids=False)
-
-    correct_edges = [
-        {"from": "a = 4", "to": "b = 2", "executed": True},
-        {"from": "b = 2", "to": "if a > b:", "executed": True},
-        {"from": "if a > b:", "to": "If.body", "executed": True},
-        {"from": "if a > b:", "to": "else:", "executed": False},
-        {"from": "If.body", "to": "finished = True", "executed": False},
-        {"from": "else:", "to": "finished = True", "executed": False},
-    ]
+    edgeCreator.display_readable_edges()
 
     assert correct_edges == edgeCreator.readable_edges
