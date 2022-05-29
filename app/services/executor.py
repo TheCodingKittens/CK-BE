94a4f3df3526efbf99926215bee194e3be5dcc32
diegobugmann <diegoricardo.bugmann@uzh.ch>
2022-05-29 18:59:50 +0200

    added error messages for functions and classes

--- app/services/executor.py
+++ app/services/executor.py
@@ -1,6 +1,5 @@
-import contextlib
 import copy
-import sys
+import types
 from io import StringIO
 from typing import List
 
@@ -12,57 +11,13 @@ Class to execute the incoming strings from the frontend
 """
 
 
-# @contextlib.contextmanager
-# def stdoutIO(stdout=None):
-#     old = sys.stdout
-#     if stdout is None:
-#         stdout = StringIO()
-#     sys.stdout = stdout
-#     yield stdout
-#     sys.stdout = old
-
-
 class Executor:
     def __init__(self):
         pass
 
-    # def exec_expression(self, endoced_expression: Base64Type):
-    #     try:
-
-    #         expression = endoced_expression.decode_str()
-    #         exec(f"""locals()['temp'] = {expression}""")
-
-    #         return locals()["temp"]
-    #     except Exception as e:
-    #         return e
-
-    # def exec_stdout(self, module: Base64Type):
-    #     try:
-    #         with stdoutIO() as s:
-    #             exec(module.decode_str())
-    #             return s.getvalue()
-
-    #     except Exception as e:
-    #         return e
-
-    # def exec_command(self, command: Base64Type):
-    #     try:
-    #         simple_expression = self.exec_expression(command)
-    #         if not isinstance(simple_expression, Exception):
-    #             return simple_expression
-    #         else:
-    #             return self.exec_module(command)
-
-    #     except Exception as e:
-    #         return e
-
     def exec_module(self, module: Base64Type):
         try:
-            # Get the Output of the module
-            # stdout = {"stdout": self.exec_stdout(module)}
 
-            # Creating the exec
-            # globalsParameter = {"__builtins__": None}
             localsParameter = {}
 
             # Get the Variables of the module
@@ -87,18 +42,9 @@ class Executor:
     # executes a module, taking a dict with variables as an input
     def exec_module_from_history(self, module: Base64Type, history: dict):
         try:
-            # Get the Output of the module
-            # stdout = {"stdout": self.exec_stdout(module)}
-
-            # Creating the exec
-            # globalsParameter = {"__builtins__": None}
             localsParameter = copy.deepcopy(history)
-
-            # Get the Variables of the module
+            # Execute the module to get the new variables
             exec(module.decode_str(), globals(), localsParameter)
-
-            # return dict(localsParameter | stdout)
-            return localsParameter
 
         except Exception as error:
             if isinstance(error, SyntaxError):
@@ -107,3 +53,20 @@ class Executor:
                     detail=f"Syntax Error: {error}, msg: {error.text}",
                 )
             raise HTTPException(status_code=400, detail=str(error))
+
+        self.check_for_classes_and_functions(localsParameter)
+
+        return localsParameter
+
+    def check_for_classes_and_functions(self, vars: dict):
+
+        # If any class or function definition is encountered -> throw an error
+        for var_value in vars.values():
+            if isinstance(var_value, type):
+                raise HTTPException(
+                    status_code=400, detail="Class definitions are not allowed!"
+                )
+            elif isinstance(var_value, types.FunctionType):
+                raise HTTPException(
+                    status_code=400, detail="Function definitions are not allowed!"
+                )
